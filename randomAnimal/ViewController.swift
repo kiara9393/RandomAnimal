c8d047b53c6de4f6dd97d4047b418667fe67af41
Chiara Amato <chiara.amato@MacBook-Pro-di-user05.local>
2018-06-05 12:12:01 +0200

    Initial Commit

---
+++ randomAnimal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  randomAnimal
+//
+//  Created by Chiara Amato on 05/06/18.
+//  Copyright © 2018 CHR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
